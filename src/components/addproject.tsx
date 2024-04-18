c6ca0d55d987029cd2b30ab6529f895e5bfe9246
Matthew <mhoenig35@gmail.com>
2024-04-18 22:28:50 +0100

    Background in add project changed

--- src/components/addproject.tsx
+++ src/components/addproject.tsx
@@ -59,7 +59,7 @@ export default function AddProject({}) {
     }
   }
   return (
-    <section className="section background m-0">
+    <section className="section backgroundTwo m-0">
     <div className="container login is-flex-grow-0 add is-max-desktop custom-border-radius p-6">
       <div className="box mt-6">
         <div className="title is-size-3 has-text-centered pl-1 mb-5">Add Project</div>
